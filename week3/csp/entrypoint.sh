40b5aadb0291f951c1ff348aa58577bf58aebbfe
nst-jhcert <jhcert@naraspace.com>
2026-03-26 15:49:13 +0900

    feat: Execute CSP server/client at entrypoint

--- week3/csp/entrypoint.sh
+++ week3/csp/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /app/libcsp
 python3 ./examples/buildall.py
+./build/zmqproxy &
+LD_LIBRARY_PATH=build PYTHONPATH=build python3 examples/python_bindings_example_server.py &
+LD_LIBRARY_PATH=build PYTHONPATH=build python3 examples/python_bindings_example_client.py -z localhost
 
 cd /app/parser
 make test
